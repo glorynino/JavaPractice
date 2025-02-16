5c35fa16ad8d3f3b22a8926bb8d0cc1c5398ee1b
Neil <neil.isso10@gmail.com>
2025-02-16 02:20:23 +0100

    2 eme EXERCICE

--- src/Exo2.java
+++ src/Exo2.java
@@ -5,10 +5,11 @@ import java.io.IOException;
 public class Exo2 {
     public static void main(String[] args) {
         File Fichier = new File("index.txt");
+        BufferedReader F =null ;
         int line = 0;
         try {
             Fichier.createNewFile();
-            BufferedReader F = new BufferedReader(new FileReader(Fichier));
+             F = new BufferedReader(new FileReader(Fichier));
 
 
             while (F.readLine()!=null) {
@@ -20,12 +21,21 @@ public class Exo2 {
             System.out.println("File not found: input.txt");
         }
         finally {
+            try {
+                if (F != null) {
+                    F.close();
+                }
+            } catch (IOException e) {
+                System.out.println("Erreur lors de la fermeture du fichier.");
+            }
             if (line == 0) {
                 System.out.println("the file is empty");
             }else {
                 System.out.println("Number of lines in the file:"+ line);
             }
+
         }
+
 
     }
 }
